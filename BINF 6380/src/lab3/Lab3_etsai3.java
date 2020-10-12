0be4f5cd6bbe08297cedfde1a4fc544ab611932a
erintsigh <59835854+erintsigh@users.noreply.github.com>
2020-10-12 14:38:23 -0400

    Fixed issues with first seqID and last sequence not displaying.

--- BINF 6380/src/lab3/Lab3_etsai3.java
+++ BINF 6380/src/lab3/Lab3_etsai3.java
@@ -2,8 +2,6 @@ package lab3;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,57 +11,53 @@ public class Lab3_etsai3 {
 	
 //	you can change the path and filename to what you want here
 	private static final String path = "/Users/erintsigh/git/UNCC-BINF-Work/BINF 6380/src/"; 
-	private static final String filename = "Mdomestica_short";
+	private static final String filename = "practice";
 	
 	
 //	this method reads the file, parses the sequence name and sequence, then runs nucleotide counts method 4x for each nucleotide, 
 //	then writes it all out to a new file. the new file will have the same path and name as the original file, but it will end in .txt
 	
 	private static void fasta_parse(String path, String filename) throws IOException
-	{
-		//List<String> name_list = new ArrayList<String>();
-		//List<String> seq_list = new ArrayList<String>();
-		
+	{	
 		BufferedReader reader = new BufferedReader(new FileReader(new File(path + filename+ ".fasta")));
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path+filename+".txt")));
 		
+//		write out the headers to the new text file, tab separated
 		writer.write("sequenceID" + "\t" + "numA" + "\t" + "numC" + "\t" + "numG" + "\t" + "numT" + "\t" + "sequence" + "\n");
 		System.out.println("sequenceID" + "\t" + "numA" + "\t" + "numC" + "\t" + "numG" + "\t" + "numT" + "\t" + "sequence" + "\n");
 		
 		String seq = "";
 		String name = "";
-		String first_name = ""; 
+		int counter = 0;
 		
-//		read the first line, blank line.
-		reader.readLine();
-		
+//		iterating over every line to read in, if the line starts with a ">" then, it moves onto the next if statement
+//		the next if statement says if the counter doesn't equal 0, then write out the name, nucleotide %'s, and sequence to the txt file
+//		the counter adds 1 each time the for loop iterates.
 		for(String nextLine = reader.readLine(); nextLine != null; nextLine = reader.readLine())
 		{
 			if(nextLine.startsWith(">"))
 			{
+				if(counter!=0)
+				{
+					writer.write(name + "\t" + nuc_number(seq,'A') + "\t" + nuc_number(seq, 'C') + "\t" + nuc_number(seq, 'G') + "\t" + nuc_number(seq, 'T') + "\t" + seq + "\n");
+					System.out.println(name + "\t" + nuc_number(seq,'A') + "\t" + nuc_number(seq, 'C') + "\t" + nuc_number(seq, 'G') + "\t" + nuc_number(seq, 'T') + "\t" + seq + "\n");
+				}
+				
 				name = (nextLine.substring(1).split(" ")[0]);
-				//System.out.println(name + "\t" + seq);
-				//name_list.add(name);
-				//first_name = name_list.get(0);
-
-				writer.write(name + "\t" + nuc_number(seq,'A') + "\t" + nuc_number(seq, 'C') + "\t" + nuc_number(seq, 'G') + "\t" + nuc_number(seq, 'T') + "\t" + seq + "\n");
-				System.out.println(name + "\t" + nuc_number(seq,'A') + "\t" + nuc_number(seq, 'C') + "\t" + nuc_number(seq, 'G') + "\t" + nuc_number(seq, 'T') + "\t" + seq + "\n");
-				//seq_list.add(seq);
 				seq = "";
 			}
 			else
 			{
 				seq = seq + nextLine.strip();
-				//seq_list.add(seq);
-				//writer.write(name + "\t" + seq + "\n");
-				//break;
 			}
 			
-				
+			counter ++;
 		}
 		
-		//System.out.println(name_list);
-		//System.out.println(seq_list.size());
+//		this line is added because the loop doesn't include the last seqID and sequence. then the reader and writer are closed.
+		writer.write(name + "\t" + nuc_number(seq,'A') + "\t" + nuc_number(seq, 'C') + "\t" + nuc_number(seq, 'G') + "\t" + nuc_number(seq, 'T') + "\t" + seq + "\n");
+		System.out.println(name + "\t" + nuc_number(seq,'A') + "\t" + nuc_number(seq, 'C') + "\t" + nuc_number(seq, 'G') + "\t" + nuc_number(seq, 'T') + "\t" + seq + "\n");
+		
 		writer.flush(); 
 		writer.close();
 		reader.close();
